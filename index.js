dbd76342321a3e4a01f995965544fe2732b08aab
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-08-17 01:00:57 +0300

    completed phase-1 javascript arrow functions lab

---
+++ index.js
@@ -0,0 +1,22 @@
+const divide = function() {
+    const num1 = 2000;
+    const num2 = 100;
+
+    const result = num1 / num2;
+    return result;
+}
+
+console.log(divide());
+
+
+const square = multiply => (multiply * multiply);
+console.log(square(2));
+
+const add = (num3, num4) => {
+    const additionResult = num3 + num4;
+    return additionResult;
+}
+
+console.log(add(3, 4));
+
+
